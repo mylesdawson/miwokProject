5ccfe5daa8d43311bc7fac25b70343605f1e0db5
Myles <mylesdawson@gmail.com>
2017-02-03 23:26:04 -0800

    Added sound samples to each of the miwok words

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,14 +5,23 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mMiwokSound = NO_SOUND_PROVIDED;
     private int mMiwokImage = NO_IMAGE_PROVIDED;
-    private static final int NO_IMAGE_PROVIDED = -1;
+    private static final int NO_IMAGE_PROVIDED = -1, NO_SOUND_PROVIDED = -1;
 
-    public Word(String defaultTranslation, String miwokTranslation, int miwokImage){
+    public Word(String defaultTranslation, String miwokTranslation, int miwokImage, int sound){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mMiwokImage = miwokImage;
+        mMiwokSound = sound;
     }
+
+    public Word(String defaultTranslation, String miwokTranslation, int miwokSound){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mMiwokSound = miwokSound;
+    }
+
 
     public Word(String defaultTranslation, String miwokTranslation){
         mDefaultTranslation = defaultTranslation;
@@ -33,6 +42,14 @@ public class Word {
 
     public boolean hasImage(){
         return mMiwokImage != NO_IMAGE_PROVIDED;
+    }
+
+    public int getMiwokSound(){
+        return mMiwokSound;
+    }
+
+    public boolean hasSound(){
+        return mMiwokSound != NO_SOUND_PROVIDED;
     }
 
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -2,7 +2,9 @@ package com.example.android.miwok;
 
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +16,15 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
+    private int mResourceID;
 
     public WordAdapter(Context context, ArrayList<Word> words) {
         super(context, 0, words);
+    }
+
+    public WordAdapter(Context context, ArrayList<Word> words, int colorResourceID) {
+        super(context, 0, words);
+        mResourceID = colorResourceID;
     }
 
     @NonNull
@@ -31,14 +39,20 @@ public class WordAdapter extends ArrayAdapter<Word> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
 
-        TextView miwokView = (TextView)convertView.findViewById(R.id.text_view_1);
-        TextView defaultView = (TextView)convertView.findViewById(R.id.text_view_2);
-        ImageView imageView = (ImageView)convertView.findViewById(R.id.image_view);
+        View textContainer = convertView.findViewById(R.id.text_container);
+        View playSoundButton = convertView.findViewById(R.id.play_button);
+        int color = ContextCompat.getColor(getContext(), mResourceID);
+        textContainer.setBackgroundColor(color);
+        playSoundButton.setBackgroundColor(color);
+
+        TextView miwokView = (TextView) convertView.findViewById(R.id.text_view_1);
+        TextView defaultView = (TextView) convertView.findViewById(R.id.text_view_2);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.image_view);
 
         miwokView.setText(word.getMiwokTranslation());
         defaultView.setText(word.getDefaultTranslation());
 
-        if(word.hasImage()){
+        if (word.hasImage()) {
             imageView.setImageResource(word.getmMiwokImage());
             imageView.setVisibility(View.VISIBLE);
         } else {
